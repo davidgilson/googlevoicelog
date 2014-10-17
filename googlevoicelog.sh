5cdbbf63afae0e80a7319e71356b0b4d6ddc137e
David Gilson <david@davidgilson.co.uk>
2014-10-17 17:07:40 -0300

    Update googlevoicelog.sh

--- googlevoicelog.sh
+++ googlevoicelog.sh
@@ -7,9 +7,9 @@ echo -e "Date\tTime(GMT)\tNumber\tType\tDuration" > $file
 for f in *.html
 do
 	tail -n 1 $file
-	d=$(grep "published" -- "$f" | awk -v FS="(>|<)" '{print $3}')
-	ddate=$(echo $d | sed 's/,/\ /g' | awk -F " " '{print $1" "$2" "$3}')
-	dtime=$(echo $d | sed 's/,/\ /g' | awk -F " " '{print $4}')
+	d=$(grep "published" -- "$f" | awk -v FS="(>|<)" '{print $3}' | sed 's/,/\ /g')
+	ddate=$(date -d "$d" +"%Y-%m-%d")
+	dtime=$(date -d "$d" +"%R")
 	n=$(grep "tel" -- "$f" | awk -v FS="(>|<)" '{print $5}')
 	t=$(grep "\"fn\"" -- "$f" | awk -F ">" '{print $NF}' | awk -F " " '{print $1}')
 	l=$(grep "duration" -- "$f" | awk -F '[()]' '{print $2}')
